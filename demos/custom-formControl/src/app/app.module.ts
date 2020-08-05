16625f425028704d61dcc98c29fcca692945cc75
liuchunfeng <chunfeng.liu@innocellence.com>
2020-08-05 09:33:06 +0800

    update remark import ReactiveFormsModule

--- demos/custom-formControl/src/app/app.module.ts
+++ demos/custom-formControl/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+// 使用 formControl 之类的指令，这个一定不要忘记 import
 import { ReactiveFormsModule } from '@angular/forms'
 
 import { AppComponent } from './app.component';
